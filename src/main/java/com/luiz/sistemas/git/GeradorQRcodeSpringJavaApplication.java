85df12c9607715a0b702df87f35a034149831bad
lukaneto <67704845+lukaneto@users.noreply.github.com>
2020-10-23 16:37:46 -0300

    Add files via upload
    primeiro commit

---
+++ src/main/java/com/luiz/sistemas/git/GeradorQRcodeSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.sistemas.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorQRcodeSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorQRcodeSpringJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiz/sistemas/git/GeradorQRcodeSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.sistemas.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorQRcodeSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
